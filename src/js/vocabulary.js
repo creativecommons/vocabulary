0a25a91b759656f7a08b1330f6269b6e699dd2c5
OmarAmeen01 <forcodingcoding@gmail.com>
2024-10-07 20:14:45 +0530

    added accordions to the design system

--- src/js/vocabulary.js
+++ src/js/vocabulary.js
@@ -1,7 +1,7 @@
 const exploreButton = document.querySelector('button.explore');
 const explorePanel = document.querySelector('.explore-panel');
 
-// explorePanel.classList.add('hide');
+explorePanel.classList.add('hide');
 
 exploreButton.addEventListener('click', (event) => {
     explorePanel.classList.toggle('expand');
@@ -29,4 +29,34 @@ if (attributionButton !== null && attributionPanel !== null ) {
         // explorePanel.classList.toggle('hide');
     });
 
+}
+
+const accordionHeader = document.querySelectorAll(".accordion-header")
+
+    if(typeof accordionHeader !== null){
+
+        accordionHeader.forEach(header=>{
+          header.addEventListener("click",(event)=>{
+
+             const content = header.nextElementSibling
+             const chevron = header.querySelector(".accordion-chevron")
+
+             if(content.classList.contains("hidden")){
+                 // removing hidden class from accordion-content
+                content.classList.toggle("hidden")
+                      // adding accordion-content class in accordion-content
+                 content.classList.toggle("accordion-content")
+                    // adding accordion-chevron-active class in accordion chevron
+                 chevron.classList.toggle("accordion-chevron-active")
+
+            }else{
+                    // removing hidden class from accordion-content
+                    chevron.classList.toggle("accordion-chevron-active") 
+                    // adding hidden class in accordion-content is it is active
+                    content.classList.toggle("hidden")
+                    // removing accordion-content class from accordion-content
+                    content.classList.toggle("accordion-content")
+             }
+        })
+    })
 }
