97274c0aa29d7c9b54776bcbbc027512bdc836fa
Timid Robot Zehta <timid@creativecommons.org>
2025-04-30 09:32:03 +0200

    add specimen index

--- netlify-deploy.sh
+++ netlify-deploy.sh
@@ -10,12 +10,28 @@ E107="$(printf "\033[107m")"    # bright white background
 
 #### FUNCTIONS ################################################################
 
+build_specimen_index() {
+    print_header 'Build Vocabulary Specimens index'
+    build_specimen_index_file > docs/specimen/index.html
+    echo 'done.'
+    echo
+}
+
+build_specimen_index_file() {
+    local _file _relative
+    head -n12 specimen/index.html
+    for _file in docs/specimen/contexts/*.html
+    do
+        _relative="${_file#docs/specimen/}"
+        echo "      <li><a href='${_relative}'>${_relative}</a></li>"
+    done
+    echo "    </ul>\n  </main>\n</body>\n</html>"
+}
 
 print_header() {
     # Print 80 character wide black on white heading with time
     printf "${E30}${E107}# %-69s$(date '+%T') ${E0}\n" "${@}"
 }
-
 
 remove_previous_from_docs() {
     # shellcheck disable=SC2016
@@ -25,7 +41,6 @@ remove_previous_from_docs() {
     echo 'done.'
     echo
 }
-
 
 publish_to_docs() {
     # shellcheck disable=SC2016
@@ -37,8 +52,8 @@ publish_to_docs() {
     echo
 }
 
-
 #### MAIN #####################################################################
 
 remove_previous_from_docs
 publish_to_docs
+build_specimen_index
