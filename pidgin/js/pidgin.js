39d343f369ecc14bf320f0b0ba788cc28c581e73
possumbilities <109087089+possumbilities@users.noreply.github.com>
2026-03-02 21:08:10 +0000

    setup baseline touch enabled menus skeleton

---
+++ pidgin/js/pidgin.js
@@ -0,0 +1,10 @@
+
+
+
+// const mediaQuery = window.matchMedia("(pointer: coarse)");
+// isTouch = mediaQuery.matches;
+
+// // listen incase the pointer changes
+// mediaQuery.addEventListener("change", (e) => {
+//     isTouchDevice = e.matches;
+// })
