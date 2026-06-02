712f7e244c3b0d599aa33bfd6317c12b6abcd7f0
possumbilities <109087089+possumbilities@users.noreply.github.com>
2026-06-02 16:22:25 +0000

    add missing scripts

--- src/js/vocabulary.js
+++ src/js/vocabulary.js
@@ -1,3 +1,15 @@
+const menusWithChildren = document.querySelectorAll('main > aside.sidebar nav > ul > li:has(ul) > a');
+
+menusWithChildren.forEach((menu) => {
+    const expandButton = document.createElement('button');
+    expandButton.classList.add('expand');
+    expandButton.classList.add('icon-replace');
+    expandButton.classList.add('fa-angle-down');
+    expandButton.textContent = 'Expand';
+
+    menu.insertAdjacentElement("afterend", expandButton);
+});
+
 
 const collapsedButtons = document.querySelectorAll('button.expand');
 
